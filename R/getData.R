ec425aa3c82510c23b046c4def75a2776ad77da2
git05 <geek05@naver.com>
2018-01-20 22:09:23 +0900

    edit

--- R/getData.R
+++ R/getData.R
@@ -6,32 +6,32 @@
 
 saram.func = function(pagecount,keyword){
   url = paste0("http://api.saramin.co.kr/job-search?keywords=",keyword,"&fields=posting-date+expiration-date+keyword-code+count&count=100&start=",pagecount)
-  url.frame = read_xml(url)
-  url = url.frame %>% xml_find_all("//url") %>% xml_text
-  name  = url.frame %>% xml_find_all("//name") %>% xml_text
-  title = url.frame %>% xml_find_all("//title") %>% xml_text
-  posting_timestamp = url.frame %>% xml_find_all("//posting-timestamp") %>% xml_text
-  modification_timestamp = url.frame %>% xml_find_all("//modification-timestamp") %>% xml_text
-  opening_timestamp = url.frame %>% xml_find_all("//opening-timestamp") %>% xml_text
-  expiration_timestamp = url.frame %>% xml_find_all("//expiration-timestamp") %>% xml_text
-  close_type = url.frame %>% xml_find_all("//expiration-timestamp") %>% xml_text
-  location_code = url.frame %>% xml_find_all("//location") %>% xml_attr("code")
-  location_name = url.frame %>% xml_find_all("//location") %>% xml_text
-  job_type_code = url.frame %>% xml_find_all("//job-type") %>% xml_attr("code")
-  job_type_name = url.frame %>% xml_find_all("//job-type") %>% xml_text
-  industry_code = url.frame %>% xml_find_all("//industry") %>% xml_attr("code")
-  industry_name = url.frame %>% xml_find_all("//industry") %>% xml_text
-  job_category_code = url.frame %>% xml_find_all("//job-category") %>% xml_attr("code")
-  job_category_name = url.frame %>% xml_find_all("//job-category") %>% xml_text
-  experience_level_code = url.frame %>% xml_find_all("//experience-level") %>% xml_attr("code")
-  experience_level_min = url.frame %>% xml_find_all("//experience-level") %>% xml_attr("min")
-  experience_level_max = url.frame %>% xml_find_all("//experience-level") %>% xml_attr("max")
-  experience_level_name = url.frame %>% xml_find_all("//experience-level") %>% xml_text
-  required_education_level_code = url.frame %>% xml_find_all("//required-education-level") %>% xml_attr("code")
+  url.frame = xml2::read_xml(url)
+  url = url.frame %>% xml2::xml_find_all("//url") %>% xml2::xml_text
+  name  = url.frame %>% xml2::xml_find_all("//name") %>% xml2::xml_text
+  title = url.frame %>% xml2::xml_find_all("//title") %>% xml2::xml_text
+  posting_timestamp = url.frame %>% xml2::xml_find_all("//posting-timestamp") %>% xml2::xml_text
+  modification_timestamp = url.frame %>% xml2::xml_find_all("//modification-timestamp") %>% xml2::xml_text
+  opening_timestamp = url.frame %>% xml2::xml_find_all("//opening-timestamp") %>% xml2::xml_text
+  expiration_timestamp = url.frame %>% xml2::xml_find_all("//expiration-timestamp") %>% xml2::xml_text
+  close_type = url.frame %>% xml2::xml_find_all("//expiration-timestamp") %>% xml2::xml_text
+  location_code = url.frame %>% xml2::xml_find_all("//location") %>% xml2::xml_attr("code")
+  location_name = url.frame %>% xml2::xml_find_all("//location") %>% xml2::xml_text
+  job_type_code = url.frame %>% xml2::xml_find_all("//job-type") %>% xml2::xml_attr("code")
+  job_type_name = url.frame %>% xml2::xml_find_all("//job-type") %>% xml2::xml_text
+  industry_code = url.frame %>% xml2::xml_find_all("//industry") %>% xml2::xml_attr("code")
+  industry_name = url.frame %>% xml2::xml_find_all("//industry") %>% xml2::xml_text
+  job_category_code = url.frame %>% xml2::xml_find_all("//job-category") %>% xml2::xml_attr("code")
+  job_category_name = url.frame %>% xml2::xml_find_all("//job-category") %>% xml2::xml_text
+  experience_level_code = url.frame %>% xml2::xml_find_all("//experience-level") %>% xml2::xml_attr("code")
+  experience_level_min = url.frame %>% xml2::xml_find_all("//experience-level") %>% xml2::xml_attr("min")
+  experience_level_max = url.frame %>% xml2::xml_find_all("//experience-level") %>% xml2::xml_attr("max")
+  experience_level_name = url.frame %>% xml2::xml_find_all("//experience-level") %>% xml2::xml_text
+  required_education_level_code = url.frame %>% xml2::xml_find_all("//required-education-level") %>% xml2::xml_attr("code")
   required_education_level_name = url.frame %>% xml_find_all("//required-education-level") %>% xml_text
-  read = url.frame %>% xml_find_all("//read-cnt") %>% xml_text
-  apply = url.frame %>% xml_find_all("//apply-cnt") %>% xml_text
-  reply = url.frame %>% xml_find_all("//reply-cnt") %>% xml_text
+  read = url.frame %>% xml2::xml_find_all("//read-cnt") %>% xml2::xml_text
+  apply = url.frame %>% xml2::xml_find_all("//apply-cnt") %>% xml2::xml_text
+  reply = url.frame %>% xml2::xml_find_all("//reply-cnt") %>% xml2::xml_text
   data = data.frame(url,
                     name,
                     title,
--- R/getPage.R
+++ R/getPage.R
@@ -4,8 +4,9 @@
 #' @import xml2
 #' @import dplyr
 
+
 pagecount.func = function(url.frame){
-  list.count = url.frame %>% xml_find_all("//jobs") %>% xml_attr("total")
+  list.count = url.frame %>% xml2::xml_find_all("//jobs") %>% xml2::xml_attr("total")
   re = ceiling(as.numeric(list.count)/100)
   list = "0"
   for(i in 2:re) {
